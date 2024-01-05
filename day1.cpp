3ddb74f5eae88b2b872f11c295a01b4b64a2e154
vijeshgowda <vijesh.496@gmail.com>
2024-01-05 15:48:46 +0000

    initial with logo

--- day1.cpp
+++ day1.cpp
@@ -1,78 +1,153 @@
 #include <fstream>
-#include <functional>
 #include <iostream>
-#include <stdio.h>
 #include <string>
 #include <unordered_set>
 #include <vector>
-
+#include <unordered_map>
 
 int getResult(std::vector<std::string> &lines);
 
-int main() {
+int main()
+{
   std::ifstream inputFile("input");
 
   std::vector<std::string> lines;
   std::string line;
 
-  if (!inputFile.is_open()) {
+  if (!inputFile.is_open())
+  {
     std::cerr << "Error opening the file." << std::endl;
     return 1;
   }
 
-  while (std::getline(inputFile, line)) {
+  while (std::getline(inputFile, line))
+  {
     // std::cout << line << std::endl;
     lines.push_back(line);
   }
 
   inputFile.close();
 
-  // std::cout << "------- Lines read from the file: -------" << std::endl;
-
-  // for (auto i = lines.begin(); i != lines.end(); ++i)
-  // {
-  //   std::cout << *i << std::endl;
-  // }
-
-  // std::cout << "------- Lines read from the file: ------- 222" << std::endl;
-
-  // for (int j = 0; j < lines.size(); j++)
-  // {
-  //   std::cout << lines[j] << std::endl;
-  // }
+  int resultmain = getResult(lines);
 
   std::cout << "---------- the sum is ---------" << std::endl;
-  std::cout << getResult(lines) << std::endl;
+  std::cout << resultmain << std::endl;
+  printf("this is the sum --- -- %d -- --- \n", resultmain);
 
   return 0;
 }
 
-int getResult(std::vector<std::string> &lines) {
+int getResult(std::vector<std::string> &lines)
+{
   int i = 0, sum = 0;
   int j = lines.size();
 
   std::unordered_set<char> digits = {'0', '1', '2', '3', '4',
                                      '5', '6', '7', '8', '9'};
 
-  for (int k = 0; k < lines.size(); k++) {
+  for (int k = 0; k < lines.size(); k++)
+  {
     std::string tempL = lines[k];
+
+    std::cout << tempL << std::endl;
+
     char iv = 'a', jv = 'a';
-    for (i = 0, j = lines[k].size() - 1; iv == 'a' || jv == 'a'; i++, j--) {
-
-      // std::cout << tempL << std::endl;
-
+    for (i = 0, j = lines[k].size() - 1; iv == 'a' || jv == 'a'; i++, j--)
+    {
       char tempi = tempL[i];
       // std::cout << tempi << std::endl;
       char tempj = tempL[j];
       // std::cout << tempj << std::endl;
 
-      if (digits.find(tempi) != digits.end() && iv == 'a') {
+      if (digits.find(tempi) != digits.end() && iv == 'a')
+      {
         iv = tempi;
         // std::cout << iv << std::endl;
       }
+      else if (iv == 'a')
+      {
+        if (i + 2 < tempL.length() && tempL.substr(i, 3) == "one")
+        {
+          iv = '1';
+        }
+        else if (i + 2 < tempL.length() && tempL.substr(i, 3) == "two")
+        {
+          iv = '2';
+        }
+        else if (i + 4 < tempL.length() && tempL.substr(i, 5) == "three")
+        {
+          iv = '3';
+        }
+        else if (i + 3 < tempL.length() && tempL.substr(i, 4) == "four")
+        {
+          iv = '4';
+        }
+        else if (i + 3 < tempL.length() && tempL.substr(i, 4) == "five")
+        {
+          iv = '5';
+        }
+        else if (i + 2 < tempL.length() && tempL.substr(i, 3) == "six")
+        {
+          iv = '6';
+        }
+        else if (i + 4 < tempL.length() && tempL.substr(i, 5) == "seven")
+        {
+          iv = '7';
+        }
+        else if (i + 4 < tempL.length() && tempL.substr(i, 5) == "eight")
+        {
+          iv = '8';
+        }
+        else if (i + 3 < tempL.length() && tempL.substr(i, 4) == "nine")
+        {
+          iv = '9';
+        }
+        // std::cout << iv << std::endl;
+      }
 
-      if (digits.find(tempj) != digits.end() && jv == 'a') {
+      if (digits.find(tempj) != digits.end() && jv == 'a')
+      {
         jv = tempj;
+        // std::cout << jv << std::endl;
+      }
+      else if (jv == 'a')
+      {
+        if (j >= 2 && tempL.substr(j - 2, 3) == "one")
+        {
+          jv = '1';
+        }
+        else if (j >= 2 && tempL.substr(j - 2, 3) == "two")
+        {
+          jv = '2';
+        }
+        else if (j >= 4 && tempL.substr(j - 4, 5) == "three")
+        {
+          jv = '3';
+        }
+        else if (j >= 3 && tempL.substr(j - 3, 4) == "four")
+        {
+          jv = '4';
+        }
+        else if (j >= 3 && tempL.substr(j - 3, 4) == "five")
+        {
+          jv = '5';
+        }
+        else if (j >= 2 && tempL.substr(j - 2, 3) == "six")
+        {
+          jv = '6';
+        }
+        else if (j >= 4 && tempL.substr(j - 4, 5) == "seven")
+        {
+          jv = '7';
+        }
+        else if (j >= 4 && tempL.substr(j - 4, 5) == "eight")
+        {
+          jv = '8';
+        }
+        else if (j >= 3 && tempL.substr(j - 3, 4) == "nine")
+        {
+          jv = '9';
+        }
         // std::cout << jv << std::endl;
       }
     }
