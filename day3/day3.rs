5a7a105fd9aa5005f6521e7b527fc08795f1602a
vijeshgowda <88636289+vijeshgowda@users.noreply.github.com>
2024-03-09 17:38:05 +0000

    rust main

---
+++ day3/day3.rs
@@ -0,0 +1,14 @@
+use std::env;
+use std::fs;
+fn main() {
+    println!("hello world");
+
+    let contents = fs::read_to_string("input").expect("Should have been able to read the file");
+
+    second_function(contents);
+}
+
+fn second_function(contents: String) {
+    let name: &str = "some name";
+    println!("this is a test {name} \n{contents}");
+}
