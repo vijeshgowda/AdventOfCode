c3309b24b1989671b455697f885db40f456d42af
vijeshgowda <88636289+vijeshgowda@users.noreply.github.com>
2024-06-27 12:32:30 +0100

    minor

--- day3/day3.rs
+++ day3/day3.rs
@@ -9,14 +9,41 @@ fn main() {
 }
 
 fn sum_adjacent_numbers(schematic: String) -> i32 {
-    let mut sum = 0;
+    let sum = 0;
 
-    let rows: Vec<&str> = schematic.trim().split('\n').collect();
+    let mut rows: Vec<&str> = schematic.trim().split('\n').collect();
+
+    let mut rows_main: Vec<&str> = Vec::new();
+
+    for row in rows.iter() {
+        rows_main.push(row.trim_end_matches('\r'));
+    }
+
     for i in 0..rows.len() {
         println!("the vector at {i} is - {:}", rows[i]);
     }
 
+    rows = rows_main.clone();
+
+    let parsed_rows: Vec<Vec<i32>> = rows
+        .iter()
+        .map(|row| {
+            row.split('.')
+                .filter_map(|substr| substr.parse::<i32>().ok())
+                .collect()
+        })
+        .collect();
+
     println!("\nthe full vector is \n {:#?}", rows);
+    println!("\nthe parsed vector is \n {:?}", parsed_rows);
+
+    println!("\nthe cleaned vector is \n {:#?}", rows_main);
+
+    println!(
+        "\nthe test character is -- {:?}",
+        rows_main[0].chars().nth(9)
+    );
+
     println!("length of vector: {}", rows.len());
 
     sum
